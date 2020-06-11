b7461b87c65c277a3ffba8247358fb6f5fe7227b
Willem Toerien <wtoerien@wtprograms.com>
2020-06-12 01:26:59 +0200

    Made some minor fixes and changes.

--- App/icompanyportal/projects/companies-api/src/lib/models/company-user.ts
+++ App/icompanyportal/projects/companies-api/src/lib/models/company-user.ts
@@ -1,8 +1,9 @@
 import { CompanyUserPermission } from './company-user-permission';
+import { User } from 'users-api';
 
 export interface CompanyUser {
   companyId?: number;
   userId?: number;
   companyUserPermissions?: CompanyUserPermission[];
-  user?: any;
+  user?: User;
 }
